e6fdf336c90d3520db1128096be75f6ca98e617c
Miles Bosch <milbosrej@alu.edu.gva.es>
2023-05-30 20:18:19 +0200

    aaaaa

--- vite.config.js
+++ vite.config.js
@@ -11,9 +11,3 @@ export default defineConfig({
     }
   }
 })
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/P/'  // Reemplaza "nombre-repositorio" con el nombre de tu repositorio
-    : '/'
-}
