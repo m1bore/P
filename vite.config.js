9bed89b4a639b4876550372a863c2f400894c47d
Miles Bosch <milbosrej@alu.edu.gva.es>
2023-05-30 20:15:47 +0200

    aaa

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/P'  // Reemplaza "nombre-repositorio" con el nombre de tu repositorio
+    : '/'
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-      ? '/P/'  // Reemplaza "nombre-repositorio" con el nombre de tu repositorio
+      ? '/P'  // Reemplaza "nombre-repositorio" con el nombre de tu repositorio
       : '/'
   }
